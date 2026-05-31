52d40e0e15e24db2787e7e8dd02db8ba649a8784
getji <getji@khu.ac.kr>
2026-05-31 09:58:22 +0900

    fix: 문제 3번 수정

--- Project-Euler/problem_003.py
+++ Project-Euler/problem_003.py
@@ -2,16 +2,15 @@
 # # 링크: https://projecteuler.net/problem=3
 # # 문제: 600851475143의 가장 큰 소인수 구하기
 
-def is_prime(a):
-    for i in range(2,a):
-        if a % i == 0:
-            return False
-    return True
+#아래의 코드에서 불필요한 전체 순회 방식을 제거하고 소인수분해 효율을 개선함.
+#소인수분해의 수학적 원리를 활용해 구현하였다.
 
-def max_prime_factor(a):
-    for i in range(a,1,-1):
-        if a % i == 0 and is_prime(i):
-            return i
-    return 1
-
-print(max_prime_factor(600851475143))
+def max_prime_factor(num):
+    d = 2
+    temp = num
+    while d ** 2 <= num:
+        if temp % d == 0:
+            temp //= d
+        else:
+            d += 1
+    return temp
